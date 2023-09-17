69c7a58084628d310a5f26d6aaa230f6aeabd34a
rocks143014 <119806738+rocks143014@users.noreply.github.com>
2023-09-17 14:37:44 +0530

    Add files via upload

--- hell_string.py
+++ hell_string.py
@@ -34,13 +34,13 @@ except:
 
 def main():
     print("T E A M    H E L L B O T   ! !")
-    print("Hello!! Welcome to HellBot Session Generator\n")
+    print("Hello!! Welcome to XD Session Generator\n")
     print("Human Verification Required !!")
     while True:
         verify = int(randint(1, 50))
         okvai = int(input(f"Enter {verify} to continue: "))
         if okvai == verify:
-            print("\nChoose the string session type: \n1. HellBot \n2. Telethon \n3. Pyrogram \n4. Instagram")
+            print("\nChoose the string session type: \n1. XD \n2. Telethon \n3. Pyrogram \n4. Instagram")
             while True:
                 library = input("\nYour Choice: ")
                 if library == "1":
@@ -63,17 +63,17 @@ def main():
 
 
 def generate_hellbot_session():
-    print("!!! HELLBOT SESSION !!!")
-    print("One session for all HellBot's Project.")
+    print("!!! XDBOT SESSION !!!")
+    print("One session for all XDBot's Project.")
     api_id = int(input("\nEnter APP ID here: "))
     api_hash = input("\nEnter API_HASH here: ")
-    with PClient(name="helluser", api_id=api_id, api_hash=api_hash, in_memory=True) as hell:
-        print("\nYour HELLBOT SESSION is saved in your telegram saved messages.")
+    with PClient(name="XDuser", api_id=api_id, api_hash=api_hash, in_memory=True) as hell:
+        print("\nYour XDBOT SESSION is saved in your telegram saved messages.")
         _session = hell.export_session_string()
         hell_session = hellbot_session(_session)
         hell.send_message(
             "me",
-            f"#HELLBOT_SESSION \n\n`{hell_session}`",
+            f"#XDBOT_SESSION \n\n`{XD_session}`",
         )
 
 
@@ -81,28 +81,28 @@ def generate_pyro_session():
     print("Pyrogram Session for Music Bot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
-    with PClient(name="helluser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as hellbot:
-        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-        hellbot.send_message(
+    with PClient(name="XDuser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as hellbot:
+        print("\nYour XDBot Session Is sent in your Telegram Saved Messages.")
+        XDbot.send_message(
             "me",
-            f"#HELLBOT #PYROGRAM\n\n`{hellbot.export_session_string()}`",
+            f"#XDBOT #PYROGRAM\n\n`{XDbot.export_session_string()}`",
         )
 
 
 def generate_telethon_session():
-    print("\nTelethon Session For HellBot!")
+    print("\nTelethon Session For XDBot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
     with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
-        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-        hellbot.send_message(
+        print("\nYour XDBot Session Is sent in your Telegram Saved Messages.")
+        XDbot.send_message(
             "me",
-            f"#HELLBOT #TELETHON \n\n`{hellbot.session.save()}`",
+            f"#XDBOT #TELETHON \n\n`{XDbot.session.save()}`",
         )
 
 
 def generate_insta_session():
-    print("Instagram Session For HellBot!")
+    print("Instagram Session For XDBot!")
     cl = IClient()
     username = input("Enter your Instagram Username: ")
     password = input("Enter your Instagram Password: ")
@@ -151,7 +151,7 @@ def hellbot_session(session):
         5: "91.108.56.130",
     }
 
-    error_msg = "Error in generating session! Report it in Hell Chats"
+    error_msg = "Error in generating session! Report it in XD Chats"
 
     # converting pyrogram session
     if len(session) in pyro_format.keys():
@@ -176,7 +176,7 @@ def hellbot_session(session):
                 auth_key
             )
         )
-        return f"=={hellbot('hell')}{new_session}{hellbot('bot')}=="
+        return f"=={XDbot('XD')}{new_session}{XDbot('bot')}=="
     else:
         return error_msg
 
