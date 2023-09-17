7996e6c97e5cd7ecac5f33e6e7df5c8b3bfdc7ce
rocks143014 <119806738+rocks143014@users.noreply.github.com>
2023-09-17 13:20:20 +0530

    Update formatters.py

--- Music/helpers/formatters.py
+++ Music/helpers/formatters.py
@@ -109,8 +109,8 @@ class Formatters:
         self,
         title: str,
         text: str,
-        auth: str = "[ †he Hêllẞø† ]",
-        url: str = "https://t.me/its_hellbot",
+        auth: str = "[ x∂ вσƚ ]",
+        url: str = "https://t.me/ROCKY_ISS_BACK",
     ):
         client = TelegraphPoster(use_api=True)
         client.create_api_token(auth)
